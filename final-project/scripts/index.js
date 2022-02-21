7b5b398aebbb80a1ff4f9c1ffd452eaf32f16285
Bethiane <ubethiane07@gmail.com>
2022-02-21 13:48:47 +0200

    cosmeta depending

---
+++ final-project/scripts/index.js
@@ -0,0 +1,6 @@
+// var cartAdded = document.getElementById("add-to-cart");
+// cartAdded.onclick = alert('wow')
+
+function addedToCart(){
+    document.getElementById("added").innerHTML = "Add to cart!";
+}
